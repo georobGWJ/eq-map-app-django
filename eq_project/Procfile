adc187f9bbabdb7e28de06dae6f8be4592753690
Robert Turner Jr <georob23@gmail.com>
2016-11-26 14:49:54 -0800

    Prepare app for heroku deployment.

---
+++ eq_project/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eq_project.wsgi --log-file -
--- eq_project/eq_project/settings.py
+++ eq_project/eq_project/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+import django.contrib.staticfiles
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
