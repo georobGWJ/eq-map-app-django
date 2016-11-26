2758660d2ae8a575e4ae7a06a3a61e91ab0c8d0a
Robert Turner Jr <georob23@gmail.com>
2016-11-26 12:09:25 -0800

    Configure settings and add libraries to store STATIC assets (images and js).

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/georob/Desktop/p2-repos-chipmunks/eq-map-app-django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
