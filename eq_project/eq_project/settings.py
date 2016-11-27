a0d91265031a9207f8737255548e58bf68493e6d
Robert Turner Jr <georob23@gmail.com>
2016-11-27 09:46:36 -0800

    Update Procfile.

--- eq_project/eq_project/settings.py
+++ eq_project/eq_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,7 +88,6 @@ DATABASES = {
 
 # HEROKU DB Configuration
 # Update database configuration with $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
--- eq_project/eq_project/wsgi.py
+++ eq_project/eq_project/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eq_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eq_project.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
