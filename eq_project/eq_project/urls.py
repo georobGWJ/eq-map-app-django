df145d424c52f527053fab7fd25de7c59bdcc2b6
Robert Turner Jr <georob23@gmail.com>
2016-11-29 11:40:46 -0800

    Implement CSS and JS file folders. Test adding partials.

--- eq_project/eq_project/urls.py
+++ eq_project/eq_project/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from eq_map import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('eq_map.urls')),
     url(r'^api-auth/', include('rest_framework.urls'))
-]
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
